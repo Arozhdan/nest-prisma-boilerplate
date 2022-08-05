f844ef2f8142513d1e70f3cab0b338a1307e37e5
arozhdan <halala30@gmail.com>
2022-08-05 14:22:24 +0200

    init

---
+++ prisma/migrations/20220805114345_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "picture" TEXT,
+    "bio" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_username_email_idx" ON "User"("username", "email");
